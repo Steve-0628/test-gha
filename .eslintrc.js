70d2578144cf8a5d37c6c8e42047ec3e3d9cff96
Steve-0628 <me@stesan.dev>
2024-03-28 13:48:18 +0900

    t

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,21 @@
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      node: true,
-    },
-    parserOptions: {
-      parser: '@babel/eslint-parser',
-      requireConfigFile: false,
-    },
-    extends: ['prettier'],
-    // add your custom rules here
-    rules: {
-      semi: [2, 'never'],
-      'no-unused-vars': 'off',
-      'no-console': 'off',
-      'no-sparse-arrays': 'off',
-      'array-callback-return': 0,
-      'import/no-named-as-default': 0,
-      // vue settings
-      'vue/script-setup-uses-vars': 0,
-      'vue/attribute-hyphenation': 0,
-      'vue/no-v-html': 0,
-      'vue/multi-word-component-names': 0,
-      // nuxt settings
-      'nuxt/no-globals-in-created': 0,
-      'nuxt/no-env-in-hooks': 0,
-    },
-  }
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['prettier'],
+  // add your custom rules here
+  rules: {
+    semi: [2, 'never'],
+    'no-unused-vars': 'off',
+    'no-console': 'off',
+    'no-sparse-arrays': 'off',
+    'array-callback-return': 0,
+    'import/no-named-as-default': 0,
+  },
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+// Hello
+function test() {
+    // this is bad
+    var myarray = [
+        1,
+        2,
+        3,
+    ]
+    console.log(arr[2])
+}
+// these blank lines are also bad
+
+
+
